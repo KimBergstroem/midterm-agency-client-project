6b579962f52f75b4180a75b606ca30138952116d
KimBergstroem <kimmen_b@hotmail.com>
2024-05-20 16:18:49 +0200

    feat: showAlert function to display success and error messages

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -8,6 +8,16 @@
 
 const POST_ENDPOINT = "https://jsonplaceholder.typicode.com/posts";
 
+const showAlert = (message, isSuccess = true) => {
+  const successAlert = document.querySelector("#contact__alert-box");
+  successAlert.style.display = "block";
+  successAlert.style.backgroundColor = isSuccess ? "green" : "red";
+  successAlert.textContent = message;
+  setTimeout(() => {
+    successAlert.style.display = "none";
+  }, 3000);
+};
+
 const sendForm = async (event) => {
   // This syntax prevents the form from refreshing the page on submission
   event.preventDefault();
@@ -35,16 +45,22 @@ const sendForm = async (event) => {
       },
     });
 
-    //Extra layer of error handling
+    // Extra layer of error handling
     if (!response.ok) {
+      // Display error message
+      showAlert("Something went wrong. Please try again!", false);
       throw new Error(`HTTP error! Status: ${response.status}`);
+    } else if (response.ok) {
+      // Display success message
+      showAlert("Your message has been sent successfully!");
     }
 
     const userResponseData = await response.json();
     console.log(userResponseData);
   } catch (e) {
+    // Display error message
     console.error("Error", e.message);
-    alert("There was an error submitting the form. Please try again.");
+    showAlert("Something went wrong. Please try again!", false);
   }
 
   formElement.reset();
