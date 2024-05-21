2064ac6813453d79ea1d9c32332ae6eba3f2a4e8
KimBergstroem <kimmen_b@hotmail.com>
2024-05-21 17:49:54 +0200

    feat: dark mode function with basic styling

---
+++ assets/js/darkMode.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("darkModeToggle");
+  // Get the current theme from localStorage
+  const currentMode = localStorage.getItem("theme");
+
+  if (currentMode === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+
+  toggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    //Defualt theme for user
+    let theme = "light";
+    if (document.body.classList.contains("dark-mode")) {
+      theme = "dark";
+    }
+    // Save the new theme to localStorage
+    localStorage.setItem("theme", theme);
+  });
+});
